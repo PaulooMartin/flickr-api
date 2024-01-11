56dc9f2b5e9eb74778660f50dc342c52ce3fe1a4
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-11 13:27:31 +0800

    Add search user and displaying user photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,22 @@
 class StaticPagesController < ApplicationController
+  before_action :user_exists?, only: [:index]
+
   def index
-    
+    if params[:flickr_id]
+      @user_photos = Flickr.new.people.getPhotosOf(user_id: params[:flickr_id])
+    end
+  end
+
+  private
+
+  def user_exists?
+    return unless params[:flickr_id]
+
+    begin
+      Flickr.new.urls.getUserProfile(user_id: params[:flickr_id])
+    rescue Flickr::FailedResponse => e
+      # the problem with this is that it doesn't print pretty.
+      redirect_to root_path, notice: e.message
+    end
   end
 end
